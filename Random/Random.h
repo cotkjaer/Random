de3084b61e7aae48d90ebd95cd8602fc5aadb5d9
christian otkjær <christian.otkjaer@gmail.com>
2016-02-14 11:37:27 +0100

    Initial Commit

---
+++ Random/Random.h
@@ -0,0 +1,19 @@
+//
+//  Random.h
+//  Random
+//
+//  Created by Christian Otkjær on 14/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Random.
+FOUNDATION_EXPORT double RandomVersionNumber;
+
+//! Project version string for Random.
+FOUNDATION_EXPORT const unsigned char RandomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Random/PublicHeader.h>
+
+
